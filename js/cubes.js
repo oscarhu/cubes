2d9bd8deb85846f6b94adcc8e05f4be864621674
Oscar Hurley <oscar@hurley.local>
2013-01-23 22:08:57 +0000

    Orbiting meshes and material with a photo

--- js/cubes.js
+++ js/cubes.js
@@ -1,4 +1,5 @@
 var camera, scene, renderer, mesh1;
+var angle = 0 ;
 
 init();
 animate();
@@ -10,31 +11,44 @@ function cube(x, y, z, color, wf) {
 }
 
 function sphere(r, color, wf) {
-  var geometry = new THREE.SphereGeometry(r,59);
-  var material = new THREE.MeshLambertMaterial( { color: color, wireframe: wf } );
+  var geometry = new THREE.SphereGeometry(r,10);
+  var material = new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture( 'oscarphoto' ) } ) ; 
   return new THREE.Mesh( geometry, material );
 }
 
+
+function orbit(satellite, body, distance, speed) {
+  if (satellite.angle === undefined) {
+    satellite.angle = 0;
+  }
+  
+  satellite.position.z = body.position.z + distance * Math.sin(satellite.angle);
+  satellite.position.x = body.position.x + distance * Math.cos(satellite.angle);
+  satellite.position.y = body.position.y;
+
+  satellite.angle += Math.PI / speed;
+  if (satellite.angle > (Math.PI * 2)) {
+    satellite.angle = 0;
+  }
+}
+
 function init() {
-  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-  camera.position.z = 500;
+  camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 1, 10050 );
+  camera.position.z = 1500;
 
   scene = new THREE.Scene()
-
-  mesh1 = cube(300, 400, 100, 0xf0f0, false);
-  scene.add( mesh1 );
-  mesh1.position.x = -300
   
-  mesh2 = cube(100, 100, 100, 0x100, false);
-  scene.add( mesh2 );
-  mesh2.position.x = 300;
-  
-  mesh3 = sphere(200, 200, 200, 0x300, true);
+  mesh3 = sphere(200, 0x300, true);
   scene.add( mesh3 );
-  mesh3.position.y = 300;
+  mesh3.position.y = 150;
+  mesh3.position.x = -300;
+
+  mesh4 = sphere(50,  0x601, false)
+  scene.add( mesh4 );
+  mesh4.position.y = -200
+  mesh4.position.x = -300
   
-  
-  renderer = new THREE.CanvasRenderer();
+  renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
 
   $("#container").append( renderer.domElement );
@@ -42,17 +56,14 @@ function init() {
 
 function animate() {
   requestAnimationFrame( animate );
-
-  mesh1.rotation.x += 0.01;
-  mesh1.rotation.y += 0.01;
-  mesh1.rotation.z += 0.01;
   
-  mesh2.rotation.x += 0.02;
-  mesh2.rotation.y += 0.02;
-  mesh2.rotation.z += 0.02;
-  
-  mesh3.rotation.x += 0.03;
+  //mesh3.rotation.x += 0.03;
   mesh3.rotation.y += 0.03;
+
+  mesh4.rotation.x += 0.04;
+  mesh4.rotation.y += 0.04;
+
+  orbit(mesh4,mesh3,700,100);
   
   renderer.render( scene, camera ); 
 }
